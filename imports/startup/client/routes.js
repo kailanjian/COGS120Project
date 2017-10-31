5ba4bdd06d920c385149720699271dc6da98b3f8
Krikor Ailanjian <kailanjian@gmail.com>
2017-10-31 10:03:56 -0700

    created skeleton of app

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -6,6 +6,9 @@ import '../../ui/layouts/body/body.js';
 import '../../ui/pages/home/home.js';
 import '../../ui/pages/not-found/not-found.js';
 import '../../ui/pages/add/add.js';
+import '../../ui/pages/view/view.js';
+import '../../ui/pages/load/load.js';
+import '../../ui/pages/camera/camera.js';
 
 // Set up all routes in the app
 FlowRouter.route('/', {
@@ -19,6 +22,27 @@ FlowRouter.route('/add', {
   name: 'App.add',
   action() {
     BlazeLayout.render('App_body', { main: 'App_add' });
+  },
+})
+
+FlowRouter.route('/view', {
+  name: 'App.view',
+  action() {
+    BlazeLayout.render('App_body', { main: 'App_view' });
+  },
+})
+
+FlowRouter.route('/load', {
+  name: 'App.load',
+  action() {
+    BlazeLayout.render('App_body', { main: 'App_load' });
+  },
+})
+
+FlowRouter.route('/camera', {
+  name: 'App.camera',
+  action() {
+    BlazeLayout.render('App_body', { main: 'App_camera' });
   },
 })
 
