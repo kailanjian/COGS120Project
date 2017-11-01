6f58a1ed2c06c3b15360c30ae4a021fbcd635ee3
Paulos Liu <paulosliu@gmail.com>
2017-10-31 23:35:53 -0700

    swag

--- imports/ui/pages/add/add.js
+++ imports/ui/pages/add/add.js
@@ -10,6 +10,22 @@ Template.camButton.events({
         e.preventDefault();
         console.log("pressed bois");
         FlowRouter.go('/camera')
+    },
+});
+
+Template.difficulty.events({
+    'click .selector_buttons': function(e){
+      console.log(e);
+      $(".difficulty_and_servings .selector_buttons").css("background-color", "inherit");
+      $(e.currentTarget).css("background-color", "#DEDEDE");
+    }
+});
+
+Template.mealOfDay.events({
+    'click .selector_buttons': function(e){
+      console.log(e);
+      $(".meal .selector_buttons").css("background-color", "inherit");
+      $(e.currentTarget).css("background-color", "#DEDEDE");
     }
 });
 
