fd3931b568b966c10b06cbd8853e204b498adeba
Krikor Ailanjian <kailanjian@gmail.com>
2017-10-31 19:36:20 -0700

    almost done with home page (nav not functional)

--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -6,16 +6,40 @@ var recipes = [{name: "recipe 1"}, {name: "recipe 2"}, {name: "recipe 3"}];
 var recipeGroups = 
   [
     {
-      name: "Random", 
-      recipes: [{name: "Random Recipe"}]
+      name: "Recommended Based on Diet Restrictions", 
+      recipes: [
+        {name: "Recommended Breakfast",
+         time: "30 min",
+         difficulty: "easy",
+         meal: "breakfast"}, 
+        {name: "Recommended Lunch",
+        time: "40 min",
+        difficulty: "hard",
+        meal: "lunch"}, 
+        {name: "Recommended Dinner",
+        time: "20 min",
+        difficulty: "med",
+        meal: "dinner"}]
     },
     {
       name: "A",
-      recipes: [{name: "Apple Pie"}, {name: "Avocado dip"}]
+      recipes: [
+        {name: "Apple Pie",
+        time: "40 min",
+        difficulty: "hard",
+        meal: "desert"}, 
+        {name: "Avocado dip",
+        time: "20 min",
+        difficulty: "med",
+        meal: "appetizer"}]
     },
     {
       name: "C",
-      recipes: [{name: "Chocolate Chip Cookies"}]
+      recipes: [
+        {name: "Chicken McNuggets",
+        time: "20 min",
+        difficulty: "easy",
+        meal: "lunch"}]
     }
   ]
 
