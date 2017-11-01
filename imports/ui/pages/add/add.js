3f489e69ee2f208fb3dce4b3cdae3e1035766821
JonLai96 <j9lai@ucsd.edu>
2017-10-31 17:52:15 -0700

    made a trashy button and want to wrap it now

--- imports/ui/pages/add/add.js
+++ imports/ui/pages/add/add.js
@@ -3,3 +3,13 @@ import './add.html';
 //import '../../components/hello/hello.js';
 //import '../../components/info/info.js';
 import '../../components/nav/nav.js';
+
+
+Template.myButton.events({
+    'click #button': function(e){
+        e.preventDefault();
+        console.log("pressed bois");
+        FlowRouter.go('/camera')
+    
+    }
+});
