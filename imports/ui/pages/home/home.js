8aa07dba4813f74438ea8c3135dfc1bd6d94f409
Krikor Ailanjian <kailanjian@gmail.com>
2017-10-31 17:20:14 -0700

    worked on home page

--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -1,5 +1,33 @@
+import { Template } from 'meteor/templating';
+
 import './home.html';
 
-//import '../../components/hello/hello.js';
-//import '../../components/info/info.js';
-import '../../components/nav/nav.js';
+var recipes = [{name: "recipe 1"}, {name: "recipe 2"}, {name: "recipe 3"}];
+var recipeGroups = 
+  [
+    {
+      name: "Random", 
+      recipes: [{name: "Random Recipe"}]
+    },
+    {
+      name: "A",
+      recipes: [{name: "Apple Pie"}, {name: "Avocado dip"}]
+    },
+    {
+      name: "C",
+      recipes: [{name: "Chocolate Chip Cookies"}]
+    }
+  ]
+
+
+Template.App_home.helpers({
+  recipeGroups() {
+    return recipeGroups;
+  }
+});
+
+Template.recipeGroup.helpers({
+  recipes() {
+    return this.recipes;
+  }
+})
