765903d63c0aa14b5f8c50dfa170ae436f9f2d86
Paulos Liu <paulosliu@gmail.com>
2017-11-07 17:14:51 -0800

    added 'done' button for deleting

--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -64,6 +64,11 @@ Template.App_home.events({
   },
   'click .removeRecipeButton'(event) {
     $('.deleteButton').show();
+    $('#done_delete').show();
+  },
+  'click #done_delete'(event) {
+    $('.deleteButton').hide();
+    $('#done_delete').hide();
   },
   'click .logoutButton'(event) {
     Meteor.logout();
@@ -116,4 +121,5 @@ Template.recipe.events({
 Template.recipe.onRendered(function() {
   // initialize states
   $('.deleteButton').hide();
+  $('#done_delete').hide();
 })
