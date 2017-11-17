c0bfe7c42e23fac44de8598de09080bbf8b9a031
Paulos Liu <paulosliu@gmail.com>
2017-11-16 16:08:23 -0800

    fixed some usability issues. hopefully doesn't break boiiiiiis

--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -202,22 +202,6 @@ Template.App_home.events({
   'click .menuButtonWrapper'(event) {
     $('.sideBarOverlay').show();
   },
-  'click #deleteRecipeButton'(event) {
-    if($('.bottom_navigation_buttons').is(":visible")){
-      $('.deleteButton').show();
-      $('.bottom_navigation_buttons').hide();
-      $('#homeDoneButton').show();
-    } else {
-      $('.deleteButton').hide();
-      $('.bottom_navigation_buttons').show();
-      $('#homeDoneButton').hide();
-    }
-  },
-  'click #homeDoneButton'(event) {
-    $('.deleteButton').hide();
-    $('#homeDoneButton').hide();
-    $('.bottom_navigation_buttons').show();
-  },
   'input .searchBar'(event) {
     let text = $(".searchInput").val();
     searchText.set(text);
@@ -248,18 +232,4 @@ Template.recipeGroup.helpers({
     else
       return 0;
   }
-})
-
-Template.recipe.events({
-  'click .deleteButton'(event) {
-    event.preventDefault();
-    console.log("minus clicked");
-    console.log(this._id);
-    Meteor.call("recipes.delete", this._id);
-  }
-})
-
-Template.recipe.onRendered(function() {
-  // initialize states
-  $('.deleteButton').hide();
 })
