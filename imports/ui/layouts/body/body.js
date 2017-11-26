a99fd6e93f347dc5e5cb3b414bc7e597b6c1a53f
Krikor Ailanjian <kailanjian@gmail.com>
2017-11-25 23:14:45 -0800

    added package for google analytics

--- imports/ui/layouts/body/body.js
+++ imports/ui/layouts/body/body.js
@@ -1,1 +1,10 @@
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { Tracker } from 'meteor/tracker'
+
 import './body.html';
+
+Template.App_body.onRendered(function() {
+  Tracker.autorun(() => {
+    document.title = FlowRouter.getRouteName();
+  });
+});
